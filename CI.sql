1e30e1a5c014b68e77a177eecc0addd96a8226a9
AliForootaninia <112258886+AliForootaninia@users.noreply.github.com>
2024-01-13 14:19:39 +0330

    translate aliases from Persian to English

--- CI.sql
+++ CI.sql
@@ -2,43 +2,43 @@
 
 -- Selecting columns from Configuration_Item (CI) table
 SELECT 
-    CI.id AS N'شناسه قلم', -- CI ID
-    CI.description AS N'عنوان قلم', -- CI Title
-    CITP.description AS N'گروه قلم', -- Parent CI Group
-    CIT.description AS N'نوع قلم', -- CI Type
-    Item.description AS N'مدل قلم', -- CI Model
-    CI.Virtual_IPAddresses AS N'آدرس IP', -- Virtual IP Address
+    CI.id AS 'CI ID', 
+    CI.description AS 'CI Title', 
+    CITP.description AS 'Parent CI Group',
+    CIT.description AS 'CI Type', 
+    Item.description AS 'CI Model', 
+    CI.Virtual_IPAddresses AS 'IP Address', 
     CASE
         WHEN CI.Virtual_DataSource = 0 THEN 'V-CENTER'
         WHEN CI.Virtual_DataSource = 1 THEN 'SOLARWINDS'
         WHEN CI.Virtual_DataSource = 2 THEN 'CISCO-PRIME'
         ELSE 'NO-SOURCE'
-    END AS N'روش شناسایی', -- Virtual Data Source Identification Method
-    (SELECT DBO.PersianDate(create_datetime) FROM History WHERE description = N'قلم پیکربندی ایجاد شد' AND History.root_pk = CI.ci_pk) AS N'تاریخ ایجاد', -- Creation Date (Persian)
-    DBO.PersianDate(CI.calibration_date) AS N'تاریخ بروز رسانی', -- Calibration Date (Persian)
+    END AS 'Identification Method', 
+    (SELECT DBO.PersianDate(create_datetime) FROM History WHERE description = 'CI Configuration Created' AND History.root_pk = CI.ci_pk) AS 'Creation Date', -- Creation Date (Persian)
+    DBO.PersianDate(CI.calibration_date) AS 'Calibration Date', -- Calibration Date (Persian)
     CASE
-        WHEN CI.operation_status = 0 THEN N'ناشناخته'
-        WHEN CI.operation_status = 1 THEN N'عادی'
-        WHEN CI.operation_status = 2 THEN N'اخطار'
-        WHEN CI.operation_status = 3 THEN N'حیاتی'
-    END AS N'وضعیت عملیاتی', -- Operational Status
-    CI.optional_number AS N'آدرس MAC', -- MAC Address
+        WHEN CI.operation_status = 0 THEN 'Unknown'
+        WHEN CI.operation_status = 1 THEN 'Normal'
+        WHEN CI.operation_status = 2 THEN 'Warning'
+        WHEN CI.operation_status = 3 THEN 'Critical'
+    END AS 'Operational Status', 
+    CI.optional_number AS 'MAC Address', 
     CASE
-        WHEN CI.status = 0 THEN N'از رده خارج'
-        WHEN CI.status = 3 THEN N'در انتظار بررسی'
-        WHEN CI.status = 4 THEN N'عملیاتی'
-    END AS N'وضعیت', -- Status
-    Case_Table.id N'شناسه مورد', -- Case ID
-    Case_Type.description N'نوع مورد', -- Case Type
-    C1.description AS N'محصول', -- Product
-    C2.description AS N'سرویس', -- Service
+        WHEN CI.status = 0 THEN 'Out of Service'
+        WHEN CI.status = 3 THEN 'Pending Investigation'
+        WHEN CI.status = 4 THEN 'Operational'
+    END AS 'Status', 
+    Case_Table.id 'Case ID', 
+    Case_Type.description 'Case Type', 
+    C1.description 'Product', 
+    C2.description 'Service', 
     CASE
-        WHEN case_table.case_status = 0 THEN N'ایجاد شده'
-        WHEN case_table.case_status = 1 THEN N'در جریان'
-        WHEN case_table.case_status = 2 THEN N'حل شده'
-        WHEN case_table.case_status = 3 THEN N'خاتمه یافته'
-    END AS 'وضعیت مورد', -- Case Status
-    COUNT(Case_CI.ci_pk) OVER (PARTITION BY Case_CI.case_pk) -- This window function retrieves the count of each CI attached to a case
+        WHEN case_table.case_status = 0 THEN 'Created'
+        WHEN case_table.case_status = 1 THEN 'In Progress'
+        WHEN case_table.case_status = 2 THEN 'Resolved'
+        WHEN case_table.case_status = 3 THEN 'Completed'
+    END AS 'Case Status', 
+    COUNT(Case_CI.ci_pk) OVER (PARTITION BY Case_CI.case_pk) 'Attached CI Count' -- This window function retrieves the count of each CI attached to a case
 FROM Configuration_Item CI 	
     LEFT JOIN Case_CI ON Case_CI.ci_pk = CI.ci_pk
     LEFT JOIN Case_Table ON Case_CI.case_pk = Case_Table.case_pk
@@ -49,4 +49,4 @@ FROM Configuration_Item CI
     LEFT JOIN Configuration_Item_Type CITP ON CITP.ci_type_pk = CIT.parent_ci_type_pk
     LEFT JOIN Item ON CI.item_pk = Item.item_pk
 WHERE 
-    CI.ci_type_pk NOT IN (83497927,83501456,83517507,83513546)
+    CI.ci_type_pk NOT IN (83497927, 83501456, 83517507, 83513546)
